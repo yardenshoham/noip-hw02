cb2ca3fd826971c8ba1513d90bf53f256f017c9b
YardenShoham <hrsi88@gmail.com>
2021-06-25 17:54:15 +0300

    Add Wolfe

--- src/unconstrained_min.py
+++ src/unconstrained_min.py
@@ -3,12 +3,13 @@ import utils
 import numpy as np
 
 
-def line_search(dir_selection_method, f, x0, step_size, obj_tol, param_tol, max_iter, init_step_len, slope_ratio, back_track_factor):
+def line_search(dir_selection_method, f, x0, step_size, obj_tol, param_tol, max_iter, init_step_len=1.0, slope_ratio=1e-4, back_track_factor=0.2):
     x_prev = x0
     f_prev, df_prev, hessian_prev = f(x0)
     path = [x_prev]
     i = 0
     success = False
+    step_alpha = step_size if dir_selection_method == 'gd' else init_step_len
     while i < max_iter:
         if dir_selection_method == 'gd':
             selected_dir = -df_prev
@@ -16,14 +17,22 @@ def line_search(dir_selection_method, f, x0, step_size, obj_tol, param_tol, max_
             selected_dir = newton_dir(hessian_prev, df_prev)
         elif dir_selection_method == 'bfgs':
             pass
-        x_next = x_prev + step_size * selected_dir
+
+        x_next = x_prev + step_alpha * selected_dir
+        f_next, df_next, hessian_next = f(x_next)
+
+        # Wolfe
+        while dir_selection_method != 'gd' and f_next > f_prev + slope_ratio * step_alpha * df_prev * selected_dir:
+            step_alpha *= back_track_factor
+            x_next = x_prev + step_alpha * selected_dir
+            f_next, df_next, hessian_next = f(x_next)
+
         step_length = np.linalg.norm(x_next - x_prev)
         if step_length < param_tol:
             success = True
             print(
                 f"Success! A small enough distance (<{param_tol}) between two consecutive iteration locations was achieved.")
             break
-        f_next, df_next, hessian_next = f(x_next)
         path.append(x_next)
         change = abs(f_next - f_prev)
         if change < obj_tol:
