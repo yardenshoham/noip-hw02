03092c2f871318792f22bbaf5d8235d1f0fdd23d
YardenShoham <hrsi88@gmail.com>
2021-06-26 13:56:54 +0300

    Add bfgs

--- src/unconstrained_min.py
+++ src/unconstrained_min.py
@@ -15,17 +15,23 @@ def line_search(dir_selection_method, f, x0, step_size, obj_tol, param_tol, max_
             selected_dir = -df_prev
         elif dir_selection_method == 'nt':
             selected_dir = newton_dir(hessian_prev, df_prev)
-        elif dir_selection_method == 'bfgs':
-            pass
 
         x_next = x_prev + step_alpha * selected_dir
         f_next, df_next, hessian_next = f(x_next)
+
+        if dir_selection_method == 'bfgs':
+            selected_dir = bfgs_dir(
+                hessian_prev, x_next, x_prev, df_next, df_prev)
+            hessian_next = selected_dir
 
         # Wolfe
         while dir_selection_method != 'gd' and f_next > f_prev + slope_ratio * step_alpha * df_prev * selected_dir:
             step_alpha *= back_track_factor
             x_next = x_prev + step_alpha * selected_dir
-            f_next, df_next, hessian_next = f(x_next)
+            if dir_selection_method == 'bfgs':
+                f_next, df_next, _ = f(x_next)
+            else:
+                f_next, df_next, hessian_next = f(x_next)
 
         step_length = np.linalg.norm(x_next - x_prev)
         if step_length < param_tol:
@@ -51,3 +57,11 @@ def line_search(dir_selection_method, f, x0, step_size, obj_tol, param_tol, max_
 
 def newton_dir(hessian_prev, df_prev):
     return np.linalg.solve(hessian_prev, -df_prev)
+
+
+def bfgs_dir(B_k, x_kplus1, x_k, nabla_f_x_kplus1, nabla_f_x_k):
+    s_k = x_kplus1 - x_k
+    y_k = nabla_f_x_kplus1 - nabla_f_x_k
+    B_kplus1 = B_k - (B_k * s_k * s_k.T * B_K) / \
+        (s_k.T * B_k * s_k) + (y_k * y_k.T) / (y_k.T * s_k)
+    return newton_dir(B_k, nabla_f_x_k)
