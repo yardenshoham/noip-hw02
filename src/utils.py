4636a19a55dab84c66f4f6ee93dd294f63796ad0
YardenShoham <hrsi88@gmail.com>
2021-07-02 13:24:21 +0300

    Add constrained plot

--- src/utils.py
+++ src/utils.py
@@ -39,3 +39,36 @@ def plot_iterations(func, path):
     plt.savefig(f"{func.__name__}_iterations.png")
     plt.clf()
     plt.cla()
+
+
+def plot_constrained(func, path, x_min):
+    func_name = func.__name__
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_title('feasible region')
+    ax.view_init(elev=19, azim=-29)
+
+    # path
+    xs, ys, zs = [], [], []
+    for x, y, z in path:
+        xs.append(x)
+        ys.append(y)
+        zs.append(z)
+    ax.plot(xs, ys, zs, label='path')
+    rounded_x = np.around(x_min[0], 2)
+    rounded_y = np.around(x_min[1], 2)
+    rounded_z = np.around(x_min[2], 2)
+    ax.scatter(x_min[0], x_min[1], x_min[2],
+               label=f'final candidate ({rounded_x}, {rounded_y}, {rounded_z})')
+
+    # feasible region
+    if func_name == "qp":
+        xs, ys, zs = [1, 0, 0], [0, 1, 0], [0, 0, 1]
+        ax.plot_trisurf(xs, ys, zs, alpha=0.3)
+
+    ax.legend()
+    plt.show()
+    plt.savefig(f"plot_constrained_{func_name}.png")
+    plt.clf()
+    plt.cla()
--- tests/test_constrained_min.py
+++ tests/test_constrained_min.py
@@ -7,13 +7,14 @@ import numpy as np
 
 class TestConstrainedMin(unittest.TestCase):
     def test_qp(self):
-        x_min = constrained_min.interior_pt(examples.qp,
-                                            [examples.qp_ineq0, examples.qp_ineq1,
-                                             examples.qp_ineq2],
-                                            np.array([[1, 1, 1]]),
-                                            np.array([1]),
-                                            np.array([0.1, 0.2, 0.7]))
+        x_min, path = constrained_min.interior_pt(examples.qp,
+                                                  [examples.qp_ineq0, examples.qp_ineq1,
+                                                   examples.qp_ineq2],
+                                                  np.array([[1, 1, 1]]),
+                                                  np.array([1]),
+                                                  np.array([0.1, 0.2, 0.7]))
         print(f"x_min={x_min}")
+        utils.plot_constrained(examples.qp, path, x_min)
 
 
 if __name__ == '__main__':
